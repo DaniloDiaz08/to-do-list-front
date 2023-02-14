6850797f554affbb46c336e88681bf1d54447128
María F. Herrera G <mariaf.herrera@outlook.com>
2023-02-13 21:18:48 -0500

    V0.1 FrontEnd development

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import todo from "./apis";
+
+import Form from "./components/Form";
+import List from "./components/List";
+
+const appTitle = "To do App";
+
+// const list = [
+//     { title: "Test # 1", completed: false },
+//     { title: "Test # 2" },
+//     { title: "Test # 3 "}
+// ];
+
+const App = () => {
+
+    const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const {data} = await todo.get("/todos");
+            setTodoList(data);
+        }
+        fetchData();
+    }, []);
+
+    const addTodo = async(item) => {
+        const {data} = await todo.post("/todos", item);
+        setTodoList((oldlist) => [...oldlist, item]);
+    }
+
+    const removeTodo = async(id) => {
+        await todo.delete(`/todos/${id}`);
+        setTodoList((oldList) => oldList.filter((item) => item._id !== id ));
+    }
+
+    const editTodo = async(id, item) => {
+        await todo.put(`/todos/${id}`, item);
+    }
+
+    return (
+        <div className="m-auto">
+            <>
+                <h1 className="font-Rubik text-dark text-center font-normal">
+                    {appTitle}
+                </h1>
+            </>
+
+            <>
+                <Form addTodo={addTodo}/>
+            </>
+
+            <>
+                <List
+                    editTodoListProp={editTodo}
+                    removeTodoListProp={removeTodo}
+                    list={todoList}
+                />
+            </>
+
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { MdAddCircle } from 'react-icons/md';
+
+const Form = ({addTodo}) => {
+
+    // We create a hook. This useState saves a string, then we use "" 
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if(inputValue.trim() === "") return;
+
+        addTodo({ title: inputValue, completed: false });
+        setInputValue("");
+    }
+
+    return (
+        <form className='w-11/12 p-5 bg-white/60 rounded-md mx-auto justify-center border-2 border-darkBlue' onSubmit={handleFormSubmit}>
+            <div className='grid grid-cols-5 w-10/12 m-auto '>
+                <div className='col-start-1 col-end-5 justify-self-center m-auto'>
+                    <input
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Escriba una tarea"
+                    className='font-Rubik font-light text-center text-darkBlue text-b py-1 border-2 border-lightBlue/60 rounded-md bg-white'>
+                    </input>
+                </div>
+                <div className='col-start-5 col-end-6 self-center'>
+                    <button type="submit" className=''>
+                        <i><MdAddCircle size={40} className='fill-darkBlue hover:fill-lightBlue'/></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default Form
